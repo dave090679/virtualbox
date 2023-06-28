08b3dd2366057a9cbae382d9b4f3ed47400fb812
David Parduhn <xkill85@gmx.net>
2023-06-28 22:27:51 +0200

    0.3: list items will now be read when arrowing.

--- addon/appModules/virtualbox.py
+++ addon/appModules/virtualbox.py
@@ -8,6 +8,9 @@ import appModuleHandler
 import controlTypes
 import api
 from NVDAObjects.UIA import UIA, ListItem
+from scriptHandler import script
+import addonHandler
+addonHandler.initTranslation()
 class VBoxList(UIA):
 	def event_UIA_elementSelected(self):
 		for child in self.children:
@@ -21,13 +24,26 @@ class VBoxList(UIA):
 				return child
 		return None
 class VBoxListItem(ListItem):
+	@script (
+		description=_("display the selected list item with nvda"),
+		gestures=["kb:downArrow","kb:upArrow"],
+		category=_("virtualbox")
+	)
+	def script_selectitem(self, gesture):
+		gesture.send()
+		for child in self.parent.children:
+			if controlTypes.State.SELECTED in child.states:
+				ui.message(child.name)
+
+
 	def event_gainFocus(self):
 		if controlTypes.State.SELECTED in self.states:
 			self.setFocus()
 
 class AppModule(appModuleHandler.AppModule):
 	def chooseNVDAObjectOverlayClasses(self, obj, clslist):
-		if obj.role == controlTypes.Role.LIST:
-			clslist.insert(0, VBoxList)
-		elif obj.role == controlTypes.Role.LISTITEM:
-			clslist.insert(0, VBoxListItem)
+		if isinstance(obj, UIA):
+			if obj.role == controlTypes.Role.LIST:
+				clslist.insert(0, VBoxList)
+			elif obj.role == controlTypes.Role.LISTITEM:
+				clslist.insert(0, VBoxListItem)
